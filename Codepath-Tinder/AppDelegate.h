f91c9f7fe12ee6d2564e9862c985d10abb814adf
Aswani Nerella <aswani521@gmail.com>
2015-11-19 14:45:23 -0800

    Initial Commit

---
+++ Codepath-Tinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Codepath-Tinder
+//
+//  Created by Aswani Nerella on 11/19/15.
+//  Copyright © 2015 Aswani Nerella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
